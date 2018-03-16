56f34765b2cb71105016561fb569524e18ca8617
teoc <teoc@tcd.ie>
2018-03-16 16:38:40 +0000

    Login and Register added.

--- app/src/main/java/com/example/ripan/map/Login.java
+++ app/src/main/java/com/example/ripan/map/Login.java
@@ -1,37 +1,38 @@
 package com.example.ripan.map;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.content.CursorLoader;
-import android.content.Intent;
-import android.content.Loader;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+        import android.animation.Animator;
+        import android.animation.AnimatorListenerAdapter;
+        import android.annotation.TargetApi;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.support.annotation.NonNull;
+        import android.support.design.widget.Snackbar;
+        import android.support.v7.app.AppCompatActivity;
+        import android.app.LoaderManager.LoaderCallbacks;
 
-import java.util.ArrayList;
-import java.util.List;
+        import android.content.CursorLoader;
+        import android.content.Loader;
+        import android.database.Cursor;
+        import android.net.Uri;
+        import android.os.AsyncTask;
 
-import static android.Manifest.permission.READ_CONTACTS;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.provider.ContactsContract;
+        import android.text.TextUtils;
+        import android.view.KeyEvent;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.view.inputmethod.EditorInfo;
+        import android.widget.ArrayAdapter;
+        import android.widget.AutoCompleteTextView;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import java.util.ArrayList;
+        import java.util.List;
+
+        import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -60,7 +61,6 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,7 +112,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
         }
         if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
             Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new OnClickListener() {
+                    .setAction(android.R.string.ok, new View.OnClickListener() {
                         @Override
                         @TargetApi(Build.VERSION_CODES.M)
                         public void onClick(View v) {
@@ -360,3 +360,5 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
     }
 }
+
+
--- app/src/main/java/com/example/ripan/map/Welcome.java
+++ app/src/main/java/com/example/ripan/map/Welcome.java
@@ -1,8 +1,12 @@
 package com.example.ripan.map;
 
 import android.content.Intent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 import android.view.View;
 import android.widget.TextView;
 
