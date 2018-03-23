284ee0ba560f7e8cb55dd1cb3458832761dc5413
teoc <teoc@tcd.ie>
2018-03-23 16:11:53 +0000

    Welcome Works - After registering, app will go straight to map interface

--- app/src/main/java/com/example/ripan/map/Welcome.java
+++ app/src/main/java/com/example/ripan/map/Welcome.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.view.View;
@@ -23,7 +24,8 @@ public class Welcome extends AppCompatActivity {
 
         //Use check for future features...
         String check = user_file.checkLogin(this);
-        if (check == "1") {
+        Log.v("Welcome", check);
+        if (check.equals("1")) {
             Intent intent = new Intent(this, NavigationDrawer.class);
             startActivity(intent);
         }
